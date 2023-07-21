48b695dc089771644b375ecc7a6c2558d46ad7de
Liubov5 <liubov.iakovleva5@gmail.com>
2023-07-21 18:44:05 +0300

    first commit

---
+++ src/store/MenuReducer.js
@@ -0,0 +1,7 @@
+import {v4 as uuidv4} from 'uuid';
+
+
+const defaultState = {
+    todos:[{id: uuidv4(), title: 'French Fries with Ketchup', img: '../images/plate__french-fries.png', price: 2.23}, {id:uuidv4(), name: "Salmon and Vegetables", price: 5.12, image:"images/plate__salmon-vegetables.png"}, {id:uuidv4(), name: "Spaghetti with Sauce", price: 7.82, image:"images/plate__spaghetti-meat-sauce.png"}, {id:uuidv4(), name: "Plate ravioli", price: 3.12, image:"images/plate__ravioli.png"}];
+}
+
