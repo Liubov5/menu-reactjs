fba88eb98fa249ae64f31a88d2d5c1f5e4e666f1
Liubov5 <liubov.iakovleva5@gmail.com>
2023-08-02 17:32:54 +0300

    modal with text

--- src/Components/Modal/ItemModal.js
+++ src/Components/Modal/ItemModal.js
@@ -1,8 +1,11 @@
 import React from "react";
 import cl from './ItemModal.module.css';
+import { useSelector } from "react-redux";
 
 export const ItemModal = ({id, visible, setVisible}) => {
     const rootClasses = [cl.myModal];
+    const a = useSelector(state => state.MenuReducer.menuList.filter(item => item.id === id));
+    console.log(a)
    if(visible){
         rootClasses.push(cl.active);
    }
@@ -11,7 +14,13 @@ export const ItemModal = ({id, visible, setVisible}) => {
         <div onClick={()=> {setVisible(false)}} className={rootClasses.join(' ')}>
             <div onClick={(e)=>{e.stopPropagation()}} className={cl.myModalContent}>
                 {/* тут надо вывести инфу об одном товаре */}
-                {id}
+                {a.length !=0  && 
+                <div>
+                    <h2>{a[0].name}</h2>
+                    <h4>{a[0].price}$</h4>
+                    <img src={require("../../"+a[0].image)} />
+                </div>
+                 }
             </div>
         </div>
     )
--- src/store/MenuReducer.js
+++ src/store/MenuReducer.js
@@ -3,7 +3,7 @@ import {v4 as uuidv4} from 'uuid';
 const CHANGE_IN_SHOPPING_CART = "CHANGE_IN_SHOPPING_CART";
 
 const defaultState = {
-    menuList:[{id: uuidv4(), name: 'French Fries with Ketchup', image: 'images/plate__french-fries.png', price: 2.23, inShoppingCart: false}, {id:uuidv4(), name: "Salmon and Vegetables", price: 5.12, image:"images/plate__salmon-vegetables.png", inShoppingCart: false}, {id:uuidv4(), name: "Spaghetti with Sauce", price: 7.82, image:"images/plate__spaghetti-meat-sauce.png", inShoppingCart: false}, {id:uuidv4(), name: "Plate ravioli", price: 3.12, image:"images/plate__ravioli.png", inShoppingCart: false}]
+    menuList:[{id: uuidv4(), name: 'French Fries with Ketchup', image: 'images/plate__french-fries.png', price: 2.23, inShoppingCart: false}, {id:uuidv4(), name: "Salmon and Vegetables", price: 5.12, image:"images/plate__salmon-vegetables.png", inShoppingCart: false}, {id:uuidv4(), name: "Spaghetti with Sauce", price: 7.82, image:"images/plate__spaghetti-meat-sauce.png", inShoppingCart: false}, {id:uuidv4(), name: "Plate ravioli", price: 3.12, image:"images/plate__ravioli.png", inShoppingCart: false}, {id: uuidv4(), name: 'French Fries with Ketchup', image: 'images/plate__french-fries.png', price: 2.23, inShoppingCart: false}]
 }
 
 export const MenuReducer = (state=defaultState, action) => {
